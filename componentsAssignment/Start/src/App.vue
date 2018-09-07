4d58b540df75c37b82c982806d4892e5b2a86403
rimanirfan <irfanriman@gmail.com>
2018-09-07 20:47:31 +0700

    remove home component

--- componentsAssignment/Start/src/App.vue
+++ componentsAssignment/Start/src/App.vue
@@ -1,9 +1,30 @@
 <template>
-    <app-dashboard></app-dashboard>
+    <div class="container">
+        <app-header></app-header>
+        <hr>
+        <div class="row">
+            <app-server-list></app-server-list> 
+            <app-server-detail></app-server-detail>
+        </div>
+        <hr>
+        <app-footer></app-footer>
+    </div>
 </template>
 
 <script>
+    import ServerList from './ServerList.vue'
+    import AppHeader from './AppHeader.vue'
+    import AppFooter from './AppFooter.vue'
+    import ServerDetail from './ServerDetails.vue'
 
+    export default {
+        components: {
+            'app-server-list'    : ServerList,
+            'app-header'         : AppHeader,
+            'app-footer'         : AppFooter,
+            'app-server-detail'  : ServerDetail
+        }
+    }
 </script>
 
 <style>
