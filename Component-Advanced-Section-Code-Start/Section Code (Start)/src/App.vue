c84e957f98a3161ac04bd4d236932858049582f4
rimanirfan <irfanriman@gmail.com>
2018-09-16 19:30:15 +0700

    finished component communication, movin on to advanced component

---
+++ Component-Advanced-Section-Code-Start/Section Code (Start)/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+               <app-quote>
+                   <h2>{{ quoteTitle }}</h2>
+                   <p>The wonderful Quote</p>
+               </app-quote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Quote from './components/Quote.vue'
+    
+    export default {
+        data: function() {
+            return {
+                quoteTitle: 'The Quote'
+            }
+        },
+        components: {
+            appQuote: Quote
+        }
+    }
+</script>
+
+<style>
+
+</style>
--- componentsCommunication/Section Code (Start)/src/components/User.vue
+++ componentsCommunication/Section Code (Start)/src/components/User.vue
@@ -3,13 +3,20 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my Name</button>
+        <p>Age is {{ age }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :name="name"></app-user-detail>
+                <app-user-detail 
+                    :name="name" 
+                    @nameWasReset="name = $event"
+                    :resetFn="resetName"
+                    :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit 
+                    :userAge="age"
+                    @ageWasEdited="age = $event"></app-user-edit>
             </div>
         </div>
     </div>
@@ -22,12 +29,16 @@
     export default {
         data: function() {
             return {
-                name: 'Max'
+                name: 'Max',
+                age: 27
             }
         },
         methods: {
             changeName() {
                 this.name = 'Riman'
+            },
+            resetName() {
+                this.name = 'Max'
             }
         },
         components: {
