1da9fcfee3cf31ff2b519fdf87054f69d4c0c3bf
rimanirfan <irfanriman@gmail.com>
2018-09-17 21:04:46 +0700

    add grid view quotes, single quote, and implement add new quote

---
+++ Wonderful Quotes - Second Project/Start/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
+</template>
+
+<script>
+
+    import QuoteGrid from './components/QuoteGrid.vue'
+    import NewQuote from './components/NewQuote.vue'
+
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just a quote to see something'
+                ],
+                maxQuotes: 10
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote)
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
