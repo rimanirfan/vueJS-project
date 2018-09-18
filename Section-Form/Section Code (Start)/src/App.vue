b5844e05627dbb9343a7f7c6317bcbd74ce30333
rimanirfan <irfanriman@gmail.com>
2018-09-18 12:16:50 +0700

    implement add message function

--- Section-Form/Section Code (Start)/src/App.vue
+++ Section-Form/Section Code (Start)/src/App.vue
@@ -39,7 +39,8 @@
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -107,7 +108,7 @@
                         <p>Mail: {{ userData.email }}</p>
                         <p>Password: {{ userData.password }}</p>
                         <p>Age: {{ userData.age }}</p>
-                        <p>Message: </p>
+                        <p style="white-space:pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
                             <li></li>
@@ -129,8 +130,9 @@
                 userData: {
                     email:'',
                     password:'',
-                    age: 27
-                }
+                    age: 27,
+                },
+                message:'A new Text'
             }
         }
     }
