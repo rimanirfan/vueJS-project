155a32819e6b9bbf1568b72367e03888f653919e
rimanirfan <irfanriman@gmail.com>
2018-09-20 19:56:38 +0700

    using multiple modifiers

--- Section-Code-Directives/Section Code (Start)/src/App.vue
+++ Section-Code-Directives/Section Code (Start)/src/App.vue
@@ -9,7 +9,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">This is a text</p>
-                <p v-local-highlight:background.delayed="'red'">This is a text</p>
+                <p v-local-highlight:background.delayed.blink="'red'">This is a text</p>
             </div>
         </div>
     </div>
@@ -24,13 +24,31 @@
                     if (binding.modifiers['delayed']) {
                         delay = 3000
                     }
-                    setTimeout(() => {
-                    if (binding.arg == 'background') {
-                        el.style.backgroundColor = binding.value
+                    if (binding.modifiers['blink']) {
+                        let mainColor = binding.value
+                        let secondColor = 'blue'
+                        let currentColor = mainColor
+
+                        setTimeout(() => {
+                            setInterval(() => {
+                                currentColor == secondColor ? currentColor = mainColor : currentColor = secondColor
+                                if (binding.arg == 'background') {
+                                    el.style.backgroundColor = currentColor
+                                } else {
+                                    el.style.color = currentColor
+                                }
+                            } ,1000)
+                        }, delay)
+
                     } else {
-                        el.style.color = binding.value
+                        setTimeout(() => {
+                        if (binding.arg == 'background') {
+                            el.style.backgroundColor = binding.value
+                        } else {
+                            el.style.color = binding.value
+                        }
+                        }, delay)
                     }
-                    }, delay)
                 }
             }
         }
