232e615732562caf7db39fd26b65de389635507a
rimanirfan <irfanriman@gmail.com>
2018-09-20 19:45:01 +0700

    using arguments and binding value on custom directive

--- Section-Code-Directives/Section Code (Start)/src/App.vue
+++ Section-Code-Directives/Section Code (Start)/src/App.vue
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
-                <p v-highlight>This is a text</p>
+                <p v-highlight:background="'red'">This is a text</p>
             </div>
         </div>
     </div>
