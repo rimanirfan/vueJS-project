7890cde227519b82083128fa7fb2011280ce07be
rimanirfan <irfanriman@gmail.com>
2018-09-20 19:59:16 +0700

    passing complex value as argument to directives

--- Section-Code-Directives/Section Code (Start)/src/App.vue
+++ Section-Code-Directives/Section Code (Start)/src/App.vue
@@ -9,7 +9,7 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custom Directives</h1>
                 <p v-highlight:background.delayed="'red'">This is a text</p>
-                <p v-local-highlight:background.delayed.blink="'red'">This is a text</p>
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', secondColor: 'green', delay: '500'}">This is a text</p>
             </div>
         </div>
     </div>
@@ -25,8 +25,8 @@
                         delay = 3000
                     }
                     if (binding.modifiers['blink']) {
-                        let mainColor = binding.value
-                        let secondColor = 'blue'
+                        let mainColor = binding.value.mainColor
+                        let secondColor = binding.value.secondColor
                         let currentColor = mainColor
 
                         setTimeout(() => {
@@ -37,15 +37,15 @@
                                 } else {
                                     el.style.color = currentColor
                                 }
-                            } ,1000)
+                            } ,binding.value.delay)
                         }, delay)
 
                     } else {
                         setTimeout(() => {
                         if (binding.arg == 'background') {
-                            el.style.backgroundColor = binding.value
+                            el.style.backgroundColor = binding.value.mainColor
                         } else {
-                            el.style.color = binding.value
+                            el.style.color = binding.value.mainColor
                         }
                         }, delay)
                     }
