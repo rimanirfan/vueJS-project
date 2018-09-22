acb3ca1cde7764446b5d48fa9dbcff855697a7a2
rimanirfan <irfanriman@gmail.com>
2018-09-22 10:39:48 +0700

    creating mixins to prevent code duplication

---
+++ Section-Code-FilterMixins/Section Code (Start)/src/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Filters & Mixins</h1>
+        <hr>
+        <input type="" v-model="filterText">
+        <ul>
+            <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {fruitMixin} from './fruitsMixin'
+
+    export default {
+        mixins: [fruitMixin]
+    }
+</script>
+
+<style>
+
+</style>
