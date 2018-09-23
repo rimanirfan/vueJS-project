f2ee048a0cc5aff5121c6b84cbcf966a50ab4b2a
rimanirfan <irfanriman@gmail.com>
2018-09-23 11:29:37 +0700

    create slide in and out transition

--- Section-Animation-Transition/Section Code (Start)/src/App.vue
+++ Section-Animation-Transition/Section Code (Start)/src/App.vue
@@ -9,6 +9,10 @@
                 <transition name="fade">
                     <div class="alert alert-info" v-if="show">Some Info</div>
                 </transition>
+
+                <transition name="slide">
+                    <div class="alert alert-info" v-if="show">Some Info</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -41,5 +45,39 @@
     .fade-leave-active {
         transition: all 0.5s;
         opacity: 0;
+    }
+
+    .slide-enter {
+        /* transform: translateY(20px) */
+    }
+
+    .slide-enter-active {
+        animation: slide-in 1s ease-out forwards;
+    }
+
+    .slide-leave {
+
+    }
+
+    .slide-leave-active {
+        animation: slide-out 1s ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateY(20px);
+        }
+        to {
+            transform: translateY(0);
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateY(0);
+        }
+        to {
+            transform: translateY(20px);
+        }
     }
 </style>
