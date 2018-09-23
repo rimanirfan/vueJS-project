186f6b8daf65ec0b3e9ca7262bdfa730604eb961
rimanirfan <irfanriman@gmail.com>
2018-09-23 11:00:39 +0700

    create fade in transition

---
+++ Section-Animation-Transition/Section Code (Start)/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <br><br>
+                <transition name="fade">
+                    <div class="alert alert-info" v-if="show">Some Info</div>
+                </transition>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                show: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .fade-enter {
+        opacity: 0;
+
+    }
+
+    .fade-enter-active {
+        transition: all 0.5s;
+    }
+
+    .fade-leave {
+
+    }
+
+    .fade-leave-active {
+        transition: all 0.5s;
+        opacity: 0;
+    }
+</style>
