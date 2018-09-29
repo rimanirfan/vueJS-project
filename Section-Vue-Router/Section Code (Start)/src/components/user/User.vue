1c2cbf1e150dd0b3284f141fc847acdc484df6d4
rimanirfan <irfanriman@gmail.com>
2018-09-29 10:53:04 +0700

    add child routes to component

--- Section-Vue-Router/Section Code (Start)/src/components/user/User.vue
+++ Section-Vue-Router/Section Code (Start)/src/components/user/User.vue
@@ -4,21 +4,13 @@
         <hr>
         <p>Loaded ID: {{ id }} </p>
         <button @click="navigateToHome" class="btn btn-primary">Go To Home</button>
+        <hr>
+        <router-view></router-view>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-                id: this.$route.params.id
-            }
-        },
-        watch: {
-            '$route'(to, from) {
-                this.id = to.params.id
-            }
-        },
         methods: {
             navigateToHome() {
                 this.$router.push('/')
