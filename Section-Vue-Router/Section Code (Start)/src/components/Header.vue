a14af3a3cf460cf7e0a88e96b1032dbf82e0e681
rimanirfan <irfanriman@gmail.com>
2018-09-29 10:45:12 +0700

    add watcher for changes in route params

--- Section-Vue-Router/Section Code (Start)/src/components/Header.vue
+++ Section-Vue-Router/Section Code (Start)/src/components/Header.vue
@@ -4,6 +4,7 @@
             to="/" 
             tag="li" 
             active-class="active" exact><a>Home</a></router-link>
-        <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+        <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link>
+        <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link>
     </ul>
 </template>
