88368bb7dbee3ca3129bc55343cf5e6d9ccee3fb
rimanirfan <irfanriman@gmail.com>
2018-09-29 15:09:13 +0700

    named router-view

--- Section-Vue-Router/Section Code (Start)/src/components/user/UserDetail.vue
+++ Section-Vue-Router/Section Code (Start)/src/components/user/UserDetail.vue
@@ -5,7 +5,7 @@
 
         <router-link 
             tag="button" 
-            :to="{ name: 'userEdit', params: { id: $route.params.id } }" 
+            :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { locale: 'en', q: 100 } }" 
             class="btn btn-primary">Edit User</router-link>
     </div>
 </template>
