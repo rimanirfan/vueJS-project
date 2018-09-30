7bf125ec5a503cfd435632f5ffe79c674e3f5437
rimanirfan <irfanriman@gmail.com>
2018-09-30 10:43:24 +0700

    beforeEnter guard to access any route

--- Section-Vue-Router/Section Code (Start)/src/components/user/UserDetail.vue
+++ Section-Vue-Router/Section Code (Start)/src/components/user/UserDetail.vue
@@ -21,6 +21,13 @@
                     hash: '#data'
                 }
             }
+        },
+        beforeRouteEnter(to, from , next) {
+            if (true) {
+                next()
+            } else {
+                next(false)
+            }
         }   
     }
 </script>
