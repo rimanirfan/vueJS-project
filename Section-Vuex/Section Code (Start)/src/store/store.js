b5c18c675b01e06d16d49d9aa846ddfafc1bfa55
rimanirfan <irfanriman@gmail.com>
2018-10-04 20:39:19 +0700

    seperate files

--- Section-Vuex/Section Code (Start)/src/store/store.js
+++ Section-Vuex/Section Code (Start)/src/store/store.js
@@ -2,27 +2,19 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import counter from './modules/counter'
 
+import * as actions from './actions'
+import * as getters from './getters'
+import * as mutations from './mutations'
+
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
     state: {
         value: 0
     },
-    getters: {
-        value: state => {
-            return state.value
-        }
-    },
-    mutations: {
-        updateValue: (state, payload) => {
-            state.value = payload
-        }
-    },
-    actions: {
-        updateValue: ({commit}, payload) => {
-            commit('updateValue', payload)
-        }
-    },
+    getters,
+    mutations,
+    actions,
      modules: {
         counter
      }
