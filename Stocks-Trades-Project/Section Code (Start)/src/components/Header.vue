afed6c6034ea5d9cd590976273fed0c2e2b2b246
rimanirfan <irfanriman@gmail.com>
2018-10-05 19:48:50 +0700

    implement navbar

--- Stocks-Trades-Project/Section Code (Start)/src/components/Header.vue
+++ Stocks-Trades-Project/Section Code (Start)/src/components/Header.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>The Header component</h1>
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav mr-auto">
+                    <router-link to="/portfolio"  activeClass="active" tag="li"><a>Portfolio</a></router-link>
+                    <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a> </router-link>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="">End day</a></li>
+                    <li class="dropdown">
+                        <a 
+                            href="" 
+                            class="dropdown-toggle"
+                            data-toggle="dropdown" role="button" aria-haspopup="true"
+                            aria-expanded="false">Save & Load <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="">Save Data</a></li>
+                            <li><a href="">Load Data</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div> <!-- Navbar collapse -->
+        </div> <!-- container-fluid -->
+    </nav>
 </template>
