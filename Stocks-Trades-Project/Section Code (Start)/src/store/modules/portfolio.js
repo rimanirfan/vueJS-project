11fbf466558a481dfbe9bc0b19be5c063c3ed122
rimanirfan <irfanriman@gmail.com>
2018-10-06 11:29:51 +0700

    implement sell stocks mutations

--- Stocks-Trades-Project/Section Code (Start)/src/store/modules/portfolio.js
+++ Stocks-Trades-Project/Section Code (Start)/src/store/modules/portfolio.js
@@ -16,5 +16,16 @@ const mutations = {
                 quantity: quantity
             })
         }
+        state.funds -= stockPrice * quantity
+    },
+    'SELL_STOCK' (state, { stockId, quantity, stockPrice}) {
+        const record = state.stocks.find(element => element.id == stockId)
+        if(record.quantity > quantity) {
+            record.quantity -= quantity
+        } else {
+            state.stocks.splice(state.stocks.indexOf(record), 1)
+        }
+        state.funds += stockPrice * quantity
+
     }
 }
